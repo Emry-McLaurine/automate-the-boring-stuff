5d064b55e79e2a9bc568907504b27f7caecfc349
Emry McLaurine <emrygm@icloud.com>
2023-01-12 20:27:29 -0500

    First Commit

---
+++ Chapter 1/hello.py
@@ -0,0 +1,15 @@
+print('Hello, world!')
+print('What is your name?') #ask for their name
+
+myName = input()
+#String concatenation and printing
+print('It is good to meet you, ' + myName)
+#printing a sentence
+print('the length of your name is: ' + str(len(myName)))
+print('What is your age?') #ask for their age
+
+myAge = input()
+print(type(myAge))
+
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
